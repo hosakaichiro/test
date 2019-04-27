590e87927ca2a860a072a3a7fa41473567e7f734
hosakaichiro <50039124+hosakaichiro@users.noreply.github.com>
2019-04-27 17:07:35 +0900

    Add files via upload

---
+++ test/Mapchip.java
@@ -0,0 +1,21 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class MapChip extends JPanel{
+
+	
+	
+	try{
+		File file = new File("C:\\java_pre\\app\\キャラチップ\\キャラチップ\\pipo-charachip021b.png");
+		BufferedImage image = ImageIO.read(file);
+		
+	}catch(IOException e){
+		
+	}
+	//
+
+}
